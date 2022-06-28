be8295bdd1b5c6e37f0a0e8af778a799fff094c9
jeongyonghee <sdf4542@naver.com>
2022-06-28 23:26:09 +0900

    JS : project filter

--- main.js
+++ main.js
@@ -64,3 +64,31 @@ document.addEventListener('scroll',()=>{
 arrow.addEventListener('click',()=>{
     scrollIntoView('#home')
 })
+
+// 프로젝트
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__project');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter==null){
+        return;
+    }
+    // forEach는
+    // let project;
+    // for(let i=0; i < projects.length; i++){
+    //     project = projects[i]
+    // }
+    projectContainer.classList.add('animation');
+    setTimeout(()=>{
+        projects.forEach((project)=>{
+            if(filter ==='*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('animation');
+    },300);
+})
